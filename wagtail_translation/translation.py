523908ca5c3d9c0721b4a4aefc695de7515ce301
Christopher Dickes <chris@statesofpop.de>
2017-10-26 21:35:18 +0200

    allow empty default language fields

--- wagtail_translation/translation.py
+++ wagtail_translation/translation.py
@@ -1,4 +1,4 @@
-from modeltranslation import settings as mt_settings
+from django.conf import settings
 from modeltranslation.translator import TranslationOptions, register
 from wagtail.wagtailcore.models import Page
 
@@ -11,4 +11,4 @@ class PageTranslationOptions(TranslationOptions):
         'seo_title',
         'search_description',
         'url_path',)
-    required_languages = getattr(mt_settings, 'REQUIRED_PAGE_LANGUAGES', None)
+    required_languages = getattr(settings, 'REQUIRED_PAGE_LANGUAGES', None)
