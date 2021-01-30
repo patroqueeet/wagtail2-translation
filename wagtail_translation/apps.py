dadfca2451fc355121cf23c5c5b121c046c4ada5
Jirka Schaefer <jirka@tschitschereengreen.com>
2021-01-30 12:02:16 +0100

    update patch to wagtail 2.11.3

--- wagtail_translation/apps.py
+++ wagtail_translation/apps.py
@@ -4,17 +4,20 @@ from django.apps import AppConfig
 
 
 class WagtailTranslationAppConfig(AppConfig):
-    name = 'wagtail_translation'
-    label = 'wagtailtranslation'
-    verbose_name = 'Wagtail translation'
+    name = "wagtail_translation"
+    label = "wagtailtranslation"
+    verbose_name = "Wagtail translation"
 
     def ready(self):
         from django.conf import settings
+
         # Add Wagtail defined fields as modeltranslation custom fields
-        setattr(settings,
-                'MODELTRANSLATION_CUSTOM_FIELDS',
-                getattr(settings, 'MODELTRANSLATION_CUSTOM_FIELDS', ()) + (
-                    'StreamField', 'RichTextField'))
+        setattr(
+            settings,
+            "MODELTRANSLATION_CUSTOM_FIELDS",
+            getattr(settings, "MODELTRANSLATION_CUSTOM_FIELDS", ())
+            + ("StreamField", "RichTextField"),
+        )
 
         # patch Site and Page models here
         from wagtail.core.models import AbstractPage, Page, Site
@@ -25,14 +28,14 @@ class WagtailTranslationAppConfig(AppConfig):
         # fix PageManager to inherit from MultilingualManager
         # since automatic manager patching no longer works (Django 1.10 and newer)
         AbstractPage.objects = MultilingualPageManager()
-        AbstractPage.objects.contribute_to_class(AbstractPage, 'objects')
+        AbstractPage.objects.contribute_to_class(AbstractPage, "objects")
         Page.objects = MultilingualPageManager()
-        Page.objects.contribute_to_class(Page, 'objects')
+        Page.objects.contribute_to_class(Page, "objects")
 
-        page_patch = import_module('wagtail_translation.page_patch')
-        site_patch = import_module('wagtail_translation.site_patch')
-        query_patch = import_module('wagtail_translation.query_patch')
-        views_patch = import_module('wagtail_translation.views_patch')
+        page_patch = import_module("wagtail_translation.page_patch")
+        site_patch = import_module("wagtail_translation.site_patch")
+        query_patch = import_module("wagtail_translation.query_patch")
+        views_patch = import_module("wagtail_translation.views_patch")
 
         for name in page_patch.__all__:
             setattr(Page, name, getattr(page_patch, name))
@@ -41,6 +44,6 @@ class WagtailTranslationAppConfig(AppConfig):
         for name in query_patch.__all__:
             setattr(PageQuerySet, name, getattr(query_patch, name))
         for name in views_patch.__all__:
-            setattr(pages, name, getattr(views_patch, name))
+            setattr(getattr(pages, name), name, getattr(views_patch, name))
 
         import wagtail_translation.signal_handlers
